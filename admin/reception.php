18834af7fc249b2e413dd0c2a7763cea0136c423
joao <walkmarxd@gmail.com>
2024-05-14 17:03:38 -0400

    diseño de formulario para registrar un documento

---
+++ admin/reception.php
@@ -0,0 +1,86 @@
+<?php
+require('header.php');
+?>
+<div class="container-fluid px-4">
+  <div class="container">
+    <h3 class="text-center">Asamblea departamental de POTOSI</h3>
+    <div id="carga" class="text-center" style="display:none;">
+      <img src="./imagenes/cargando.gif" />
+    </div>
+    <div id="formulario" style="display:block">
+      <form id="registrarentrada" method="post" enctype="multipart/form-data">
+        <div class="form-row">
+          <div class="col">
+            <div class="form-group">
+              <div class="form-group row">
+                <label for="origen" class="col-sm-2 col-form-label">origen</label>
+                <div class="col-sm-10">
+                  <textarea class="form-control" style="text-transform: uppercase;" id="origen" name="origen" rows="1"
+                    required data-toggle="popover" data-content="Este campo es requerido"></textarea>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="destinatario" class="col-sm-2 col-form-label">destinatario</label>
+                <div class="col-sm-10">
+                  <textarea class="form-control" style="text-transform: uppercase;" id="destinatario"
+                    name="destinatario" rows="1" required data-toggle="popover"
+                    data-content="Este campo es requerido"></textarea>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="instruccion" class="col-sm-2 col-form-label">instruccion</label>
+                <div class="col-sm-10">
+                  <textarea class="form-control" style="text-transform: uppercase;" id="instruccion" name="instruccion"
+                    rows="1" required data-toggle="popover" data-content="Este campo es requerido"></textarea>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="asunto" class="col-sm-2 col-form-label">asunto</label>
+                <div class="col-sm-10">
+                  <textarea class="form-control" style="text-transform: uppercase;" id="asunto" name="asunto" rows="1"
+                    required data-toggle="popover" data-content="Este campo es requerido"></textarea>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="descripcion" class="col-sm-2 col-form-label">Descripcion</label>
+                <div class="col-sm-10">
+                  <textarea class="form-control" style="text-transform: uppercase;" id="descripcion" name="descripcion"
+                    rows="3" required data-toggle="popover" data-content="Este campo es requerido" required></textarea>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="nhojas" class="col-sm-2 col-form-label">numero de hojas</label>
+                <div class="col-sm-10">
+                  <input type="number" style="text-transform: uppercase;" class="form-control" id="nhojas" name="nhojas"
+                    required data-toggle="popover" data-content="Este campo es requerido">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="archivo" class="col-sm-2 col-form-labell">Adjuntar</label>
+                <div class="col-sm-10">
+                  <input class="form-control" accept="application/pdf" type="file" id="archivo" name="archivo" required
+                    data-toggle="popover" data-content="Este campo es requerido" required>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="col">
+            <button type="submit" class="btn btn-primary   btn-block">Registrar</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+
+
+
+
+
+
+
+</div>
+<?php
+require('footer.php');
+?>
